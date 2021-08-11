d15ece58ce2afa99076edfd5758c6206ac597d16
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-08-11 22:42:23 +1000

    Completed Assignment

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,36 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react' 
+
+export default class YouTubeDebugger extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            errors: [], 
+            user: null, 
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }}
+        changeBitrate = () => (
+            this.setState({settings:
+                {...this.state.settings, 
+                bitrate: 12}})
+        )
+        changeResolution = () => (
+            this.setState({settings:
+                {...this.state.settings, 
+                video: 
+                {resolution: '720p'}}})
+        )
+
+        render () {
+            return(
+                <div>
+                    <button onClick = {this.changeBitrate}>Change Bitrate</button>
+                    <button onClick = {this.changeResolution}>Change Resolution</button>
+                </div>
+            )}
+
+    }
